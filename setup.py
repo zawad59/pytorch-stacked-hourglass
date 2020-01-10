215b5160f00dd7671b03985d05f83de0a6a7748a
Aiden Nibali <dismaldenizen@gmail.com>
2020-01-10 14:11:57 +1100

    v1.0.0a10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytorch-stacked-hourglass',
-    version='1.0.0a9',
+    version='1.0.0a10',
     maintainer='Aiden Nibali',
     url='https://github.com/anibali/pytorch-stacked-hourglass',
     packages=find_packages('src'),
