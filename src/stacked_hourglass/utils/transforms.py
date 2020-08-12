9f7b92f6f16bb698bf901c1a93a987fe77d65e20
Josh <millward678@gmail.com>
2020-08-12 12:34:22 +1000

    Decoupled scale in both Height & Width dimension. Corrected x,y,W,H / y,x,H,W inconsistencies
    In the case of square input, the scale will be the same in both axes, so can be passed in as a singular value

--- src/stacked_hourglass/utils/transforms.py
+++ src/stacked_hourglass/utils/transforms.py
@@ -41,12 +41,16 @@ def get_transform(center, scale, res, rot=0):
     """
     General image processing functions
     """
+    if not isinstance(scale, (list, torch.Tensor)):
+        scale = [scale, scale]
+
     # Generate transformation matrix
-    h = 200 * scale
+    h = 200 * scale[0]
+    w = 200 * scale[1]
     t = np.zeros((3, 3))
-    t[0, 0] = float(res[1]) / h
+    t[0, 0] = float(res[1]) / w
     t[1, 1] = float(res[0]) / h
-    t[0, 2] = res[1] * (-float(center[0]) / h + .5)
+    t[0, 2] = res[1] * (-float(center[0]) / w + .5)
     t[1, 2] = res[0] * (-float(center[1]) / h + .5)
     t[2, 2] = 1
     if not rot == 0:
@@ -78,38 +82,48 @@ def transform(pt, center, scale, res, invert=0, rot=0):
 
 
 def transform_preds(coords, center, scale, res):
-    # size = coords.size()
-    # coords = coords.view(-1, coords.size(-1))
-    # print(coords.size())
     for p in range(coords.size(0)):
         coords[p, 0:2] = to_torch(transform(coords[p, 0:2], center, scale, res, 1, 0))
     return coords
 
 
 def crop(img, center, scale, res, rot=0):
+    """
+    img: torch.Tensor: shape: C,H,W
+    center: torch.Tensor: shape: x, y
+    scale: [H/200, W/200]
+    res: H,W
+    """
+    if not isinstance(scale, (list, torch.Tensor)):
+        scale = [scale, scale]
+
     img = im_to_numpy(img)
 
     # Preprocessing for efficient cropping
     ht, wd = img.shape[0], img.shape[1]
-    sf = scale * 200.0 / res[0]
-    if sf < 2:
-        sf = 1
+    # If we set the scale properly at input, then this should be the same per-dimension
+    sf = [scale[0] * 200.0 / res[0], scale[1] * 200.0 / res[1]]
+
+    # NOTE: This seems to be handled, but is untested
+    if sf[0] < 2 or sf[1] < 2:
+        sf = [1, 1]
     else:
-        new_size = int(np.math.floor(max(ht, wd) / sf))
-        new_ht = int(np.math.floor(ht / sf))
-        new_wd = int(np.math.floor(wd / sf))
-        if new_size < 2:
+        new_size = int(np.math.floor(max(ht, wd) / sf[0]))
+        new_ht = int(np.math.floor(ht / sf[0]))
+        new_wd = int(np.math.floor(wd / sf[1]))
+        if new_size < 2:    # This returns a fully black image...
             return torch.zeros(res[0], res[1], img.shape[2]) \
                         if len(img.shape) > 2 else torch.zeros(res[0], res[1])
         else:
             img = imresize(img, [new_ht, new_wd])
-            center = center * 1.0 / sf
-            scale = scale / sf
+            center = center * 1.0 / torch.tensor(sf[::-1])  # Center is x, y. Sf is H,W
+            scale = [scale[0] / sf[0], scale[1] / sf[1]]
 
     # Upper left point
     ul = np.array(transform([0, 0], center, scale, res, invert=1))
     # Bottom right point
-    br = np.array(transform(res, center, scale, res, invert=1))
+    # NOTE: res stored as H, W. We want coordinates in x, y
+    br = np.array(transform(res[::-1], center, scale, res, invert=1))
 
     # Padding so that when rotated proper amount of context is included
     pad = int(np.linalg.norm(br - ul) / 2 - float(br[1] - ul[1]) / 2)
